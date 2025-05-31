03c758d1cff60f5d4fe097e8ca43eb3632b5e79b
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-06-01 01:55:59 +0400

    Update Program.cs

--- Lower_Upper_Bound/Program.cs
+++ Lower_Upper_Bound/Program.cs
@@ -14,27 +14,18 @@ public class Program
         numbers.Add(8);
         numbers.Add(9);
 
-        Console.WriteLine(LowerBound<int>(numbers, 2));
-        Console.WriteLine(UpperBound<int>(numbers, 2));
+        Console.WriteLine(LowerBound<int>(numbers, 9));
+        Console.WriteLine(UpperBound<int>(numbers, 9));
     }
     public static int LowerBound<T>(List<T> items, T target) where T : IComparable
     {
-        if (target.CompareTo(items[0]) == 0)
-        {
-            return 0;
-        }
-
         int left = 0, right = items.Count - 1, middle;
 
         while (right >= left)
         {
-            middle = left + ((right - left) / 2);
+            middle = left + (right - left) / 2;
 
-            if (target.CompareTo(items[middle]) == 0 && target.CompareTo(items[middle - 1]) > 0)
-            {
-                return middle;
-            }
-            else if (target.CompareTo(items[middle]) <= 0)
+             if (target.CompareTo(items[middle]) <= 0)
             {
                 right = middle - 1;
             }
@@ -43,27 +34,19 @@ public class Program
                 left = middle + 1;
             }
         }
-        return -1;
+
+        return left;
     }
 
     public static int UpperBound<T>(List<T> items, T target) where T : IComparable
     {
-        if (target.CompareTo(items[items.Count - 1]) == 0)
-        {
-            return items.Count - 1;
-        }
-        
         int left = 0, right = items.Count - 1, middle;
 
         while (right >= left)
         {
-            middle = left + ((right - left) / 2);
+            middle = left + (right - left) / 2;
 
-            if (target.CompareTo(items[middle]) == 0 && target.CompareTo(items[middle + 1]) < 0)
-            {
-                return middle;
-            }
-            else if (target.CompareTo(items[middle]) >= 0)
+            if (items[middle].CompareTo(target) <= 0)
             {
                 left = middle + 1;
             }
@@ -72,6 +55,6 @@ public class Program
                 right = middle - 1;
             }
         }
-        return -1;
+        return right + 1;
     }
 }
